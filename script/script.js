2304db99b045d08985082b71dbe0411c62b6944c
Jarosław Królik <madmax@onet.eu>
2024-05-29 21:57:42 +0200

    init commit

---
+++ script/script.js
@@ -0,0 +1,17 @@
+const burgerIcon = document.querySelector(".header-navigation-hamburger");
+const menu = document.querySelector(".header-navigation");
+const date = document.querySelector(".footer-text-date");
+const menuItems = document.querySelectorAll(
+  ".header-navigation-container-item"
+);
+
+burgerIcon.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+menuItems.forEach((e) =>
+  e.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  })
+);
+date.textContent = new Date().getFullYear();
