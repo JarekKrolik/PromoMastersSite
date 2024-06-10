f63d518082411e893598a93c22cd01ba9ff359a3
Jarosław Królik <madmax@onet.eu>
2024-06-10 21:57:43 +0200

    finish

--- process_form.php
+++ process_form.php
@@ -62,7 +62,7 @@
             $email = htmlspecialchars($_POST['email']);
             $message = htmlspecialchars($_POST['message']);
 
-            // Adres e-mail do wysłania wiadomości
+            // Adres e-mail do wysyłania wiadomości
             $to = "promoleaders@vp.pl";
             $subject = "Nowa wiadomość z formularza kontaktowego";
             $body = "Imię: $firstName\nNazwisko: $lastName\nEmail: $email\n\nWiadomość:\n$message";
@@ -75,80 +75,9 @@
             $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
             $headers .= "Content-Transfer-Encoding: 8bit\r\n";
 
-            // Flagi do sprawdzenia statusu wysyłki
-            $mainEmailSent = false;
-            $confirmationEmailSent = false;
-
             // Wyślij wiadomość na adres promoleaders@vp.pl
             if (mail($to, $subject, $body, $headers)) {
-                $mainEmailSent = true;
-            }
-
-            // Wyślij potwierdzenie na adres e-mail wpisany w formularzu
-            $confirmSubject = "Potwierdzenie wysłania wiadomości";
-            $confirmBody = "
-            <html>
-            <head>
-                <meta charset='UTF-8'>
-                <style>
-                    body {
-                        font-family: 'Alegreya Sans', sans-serif;
-                        background-color: #f4f4f4;
-                        padding: 20px;
-                    }
-                    .container {
-                        background-color: beige;
-                        border: 1px solid black;
-                        box-shadow: 3px 3px 3px black;
-                        padding: 20px;
-                        border-radius: 8px;
-                        text-align: center;
-                    }
-                    h1 {
-                        text-transform: uppercase;
-                        font-size: 2.6rem;
-                    }
-                    p {
-                        font-size: 1.6rem;
-                    }
-                    .highlight {
-                        font-weight: bold;
-                        text-transform: uppercase;
-                    }
-                </style>
-            </head>
-            <body>
-                <div class='container'>
-                    <h1>Potwierdzenie wysłania wiadomości</h1>
-                    <p>Dziękujemy za kontakt, <span class='highlight'>$firstName $lastName</span>.</p>
-                    <p>Otrzymaliśmy Twoją wiadomość:</p>
-                    <p>$message</p>
-                </div>
-            </body>
-            </html>";
-
-            // Dodanie nagłówków dla potwierdzenia
-            $confirmHeaders = "From: $to\r\n";
-            $confirmHeaders .= "Reply-To: $to\r\n";
-            $confirmHeaders .= "Return-Path: $to\r\n";
-            $confirmHeaders .= "MIME-Version: 1.0\r\n";
-            $confirmHeaders .= "Content-Type: text/html; charset=UTF-8\r\n";
-            $confirmHeaders .= "Content-Transfer-Encoding: 8bit\r\n";
-
-            if (mail($email, $confirmSubject, $confirmBody, $confirmHeaders)) {
-                $confirmationEmailSent = true;
-            }
-
-            // Wyświetl komunikaty
-            if ($mainEmailSent && $confirmationEmailSent) {
-                echo "<p>Wiadomość została wysłana pomyślnie.</p>";
-                echo "<p>Potwierdzenie zostało wysłane na Twój adres e-mail.</p>";
-            } elseif ($mainEmailSent) {
-                echo "<p>Wiadomość została wysłana pomyślnie.</p>";
-                echo "<p>Wystąpił problem podczas wysyłania potwierdzenia.</p>";
-            } elseif ($confirmationEmailSent) {
-                echo "<p>Wystąpił problem podczas wysyłania wiadomości głównej.</p>";
-                echo "<p>Potwierdzenie zostało wysłane na Twój adres e-mail.</p>";
+                echo "<p>Dziękujemy za twoją wiadomość, postaramy się odpowiedzieć najszybciej jak to będzie możliwe.</p>";
             } else {
                 echo "<p>Wystąpił problem podczas wysyłania wiadomości.</p>";
             }
@@ -156,7 +85,7 @@
             echo "<p>Nieprawidłowe żądanie.</p>";
         }
         ?>
-        <button onclick="window.location.href='index.html#contact'">OK</button>
+        <button onclick="window.location.href='index.html#kontakt'">OK</button>
     </div>
 </body>
 
